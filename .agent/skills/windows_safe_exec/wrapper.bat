ca2fc681d5d584e76c56befd8ecacd8aa415e65b
rieszedit <rieszedit@gmail.com>
2026-01-22 13:44:54 +0900

    Fix: Stop tracking ignored files

--- .agent/skills/windows_safe_exec/wrapper.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Helper script to execute commands safely by ignoring trailing garbage (quotes)
-REM Usage: wrapper.bat "your command here"
-REM Example: wrapper.bat "git status"
-
-%~1 & REM
